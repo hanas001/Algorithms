3deee7a3964e5c0e9d0f10239aded8a294cba314
hanas001 <hanas001@ya.ru>
2020-12-18 12:06:56 +0300

    added comments

--- 2.2.6.py
+++ 2.2.6.py
@@ -6,24 +6,24 @@
 
 import random
 
-#n = random.randrange(1, 40, 1)     #random range between 1 and 40, step 1
-n = 30                              #for the tests
-#n = int(input("Type number "))     #user input
+#n = random.randrange(1, 40, 1)                         #random range between 1 and 40, step 1
+n = 30                                                  #for the tests
+#n = int(input("Type number "))                         #user input
 
 
 def fibonacciNumbers(n):
-    numbers = [ 1 , 1 ]     #Fibonacci sequience starts with
+    numbers = [ 1 , 1 ]                                 #Fibonacci sequience starts with
     nextNum = 0
-    if n >= 2:                  #calculate only if n is more then 2
-        for i in range(n):      #range of n steps
-            if i >= 2:          #add calculated number to the list only starting after second step
+    if n >= 2:                                          #calculate only if n is more then 2
+        for i in range(n):                              #range of n steps
+            if i >= 2:                                  #add calculated number to the list only starting after second step
                 nextNum = numbers[i-1] + numbers[i-2]   #calculate next number in a line
                 numbers.append(nextNum)                 #append calculated number to the list
         return (numbers[-1])                            #return last number
     elif n == 1:
-        return numbers [ -1 ]           #return last number from the list
+        return numbers [ -1 ]                           #return last number from the list
     elif n == 0:
-        return numbers [ 0 ]            #return 0 position from the list
+        return numbers [ 0 ]                            #return 0 position from the list
 
 #print ("Full Fibonacci line", numbers)
 print ("Fibonacci number at step", n, "is", fibonacciNumbers(n))
